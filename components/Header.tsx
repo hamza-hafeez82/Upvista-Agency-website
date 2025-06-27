19e6585472a853dfdd6ba34ad65ad17788209148
Upvista <upvistadigital@gmail.com>
2025-06-27 12:49:02 +0500

    feat

--- components/Header.tsx
+++ components/Header.tsx
@@ -195,6 +195,13 @@ const Header = () => {
             <span>Services</span>
             <span className="absolute bottom-0 left-0 w-full h-0.5 bg-gradient-to-r from-indigo-500 to-purple-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
           </Link>
+          <Link
+            href="/pages/portfolio"
+            className="text-white/90 hover:text-white px-3 py-2 text-sm font-medium transition-all duration-300 relative group"
+          >
+            <span>Portfolio</span>
+            <span className="absolute bottom-0 left-0 w-full h-0.5 bg-gradient-to-r from-indigo-500 to-purple-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+          </Link>
 
           {/* Community Link */}
           <Link
